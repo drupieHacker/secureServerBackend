6e9a88c1d7e568ddf667563ffe2ed52652a2b85b
saul <spydark392@gmail.com>
2022-06-28 16:43:57 -0500

    version with files in the site web

---
+++ form.php
@@ -0,0 +1,17 @@
+<?php
+$nombre=$_REQUEST[nombre];
+$correo=$_REQUEST[correo];
+$pregunta=$_REQUEST[pregunta];
+
+ /*  $conexion = mysqli_connect("localhost", "root", "", "secure") or
+    die("Problemas con la conexión"); */
+    $conexion = mysqli_connect("localhost", "seguri29_leads", "qjOT!t46Q8v9", "seguri29_leads") or
+    die("Problemas con la conexión");
+  mysqli_query($conexion, "insert into registros(nombre,correo,pregunta) values 
+                       ('$nombre','$correo','$pregunta')")
+    or die("Problemas en el select" . mysqli_error($conexion));
+
+  mysqli_close($conexion);
+  header("Location:feed.html");
+ 
+  ?>
