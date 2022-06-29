b51ecf372c70d831f8b87786f21c445407782c28
saul <spydark392@gmail.com>
2022-06-29 12:05:03 -0500

    cambio de menu con compatibilidad de hosting hecho con jquery & HTML5 CSS3

--- script.js
+++ script.js
@@ -1,41 +1,16 @@
-
-let navbar = document.querySelector(".navbar");
-let searchBox = document.querySelector(".search-box .bx-search");
-
-
-searchBox.addEventListener("click", ()=>{
-  navbar.classList.toggle("showInput");
-  if(navbar.classList.contains("showInput")){
-    searchBox.classList.replace("bx-search" ,"bx-x");
-  }else {
-    searchBox.classList.replace("bx-x" ,"bx-search");
-  }
-});
-
-
-let navLinks = document.querySelector(".nav-links");
-let menuOpenBtn = document.querySelector(".navbar .bx-menu");
-let menuCloseBtn = document.querySelector(".nav-links .bx-x");
-menuOpenBtn.onclick = function() {
-navLinks.style.left = "0";
-}
-menuCloseBtn.onclick = function() {
-navLinks.style.left = "-100%";
-}
-
-
-
-let htmlcssArrow = document.querySelector(".htmlcss-arrow");
-htmlcssArrow.onclick = function() {
- navLinks.classList.toggle("show1");
-}
-let moreArrow = document.querySelector(".more-arrow");
-moreArrow.onclick = function() {
- navLinks.classList.toggle("show2");
-}
-let jsArrow = document.querySelector(".js-arrow");
-jsArrow.onclick = function() {
- navLinks.classList.toggle("show3");
-}
-
+$( document ).ready(function() {
+
+  $('nav ul li > a:not(:only-child)').click(function(e) {
+    $(this).siblings('.nav-submenu').toggle();
+    $('.nav-submenu').not($(this).siblings()).hide();
+    e.stopPropagation();
+  });
   
+  $('html').click(function() {
+    $('.nav-submenu').hide();
+  });
+  $('#nav-boton').click(function(){
+      $('nav ul').toggle()
+      $('#nav-boton').toggleClass("activo");
+  })
+  });
